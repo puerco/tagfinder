6d75a32b2a1cc1a45e1e35e357ff618912b47c4a
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-12-13 21:25:59 -0600

    Add options builder method
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- pkg/scanner/options.go
+++ pkg/scanner/options.go
@@ -7,6 +7,14 @@ type Options struct {
 
 type FnOption func(*Options)
 
+func buildOptions(passedOpts []FnOption) *Options {
+	opts := &Options{}
+	for _, o := range passedOpts {
+		o(opts)
+	}
+	return opts
+}
+
 func WithThreads(tNum int) FnOption {
 	return func(o *Options) {
 		o.Threads = tNum
